77c114b4ba373763612dc2f418ceaa04700f5e52
Jacob Middleton <109218193+JacobMiddleton@users.noreply.github.com>
2022-11-04 07:27:16 -0400

    Add call to renderAction()

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -19,7 +19,7 @@ export class Track extends React.Component {
     <h3>{this.props.track.name}</h3> 
     <p>{this.props.track.artist} | {this.props.track.album}</p>
   </div>
-  {/* <button className="Track-action"><!-- + or - will go here --></button> */}
+  {this.renderAction()};
 </div>
         )
     }
