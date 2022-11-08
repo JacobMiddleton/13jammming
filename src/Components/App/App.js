6fcd00f3c8ba1d170174c46ea6c6b7db62dfd218
Jacob Middleton <109218193+JacobMiddleton@users.noreply.github.com>
2022-11-08 09:35:56 -0500

    Add bind savePlaylist & pass it to Playlist

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -24,6 +24,7 @@ class App extends React.Component {
     }
     this.removeTrack = this.removeTrack.bind(this);
     this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
   }
   
   addTrack(track) {
@@ -62,6 +63,7 @@ class App extends React.Component {
       playlistTracks={this.state.playlistTracks} 
       onRemove={this.removeTrack}
       onNameChange={this.updatePlaylistName}
+      onSave={this.savePlaylist}
   />
     </div>
   </div>
