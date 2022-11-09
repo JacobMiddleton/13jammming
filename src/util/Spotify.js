397be59d5e8340f4dbfba051c91081d8bb98fd6f
Jacob Middleton <109218193+JacobMiddleton@users.noreply.github.com>
2022-11-09 11:02:52 -0500

    Add functionality to fetch()

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -24,7 +24,8 @@ const Spotify = {
         }
     },
     search(searchTerm) {
-
+        const accessToken = Spotify.getAccessToken();
+        return fetch(`https://api.spotify.com/v1/search?type=track&q=${searchTerm}`, { headers: {Authorization: `Bearers ${accessToken}`}})
     }
 };
 
