da572655338c88e44f08191bce8b9f4d9bd4abbb
Jacob Middleton <109218193+JacobMiddleton@users.noreply.github.com>
2022-11-09 11:41:10 -0500

    Add return a GET request

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -24,8 +24,10 @@ const Spotify = {
         }
     },
     search(searchTerm) {
-        const accessToken = Spotify.getAccessToken();
-        return fetch(`https://api.spotify.com/v1/search?type=track&q=${searchTerm}`, { headers: {Authorization: `Bearers ${accessToken}`}})
+    const accessToken = Spotify.getAccessToken();
+    return fetch(
+        `https://api.spotify.com/v1/search?type=track&q=${searchTerm}`, { headers: {Authorization: `Bearers ${accessToken}`}}
+        )
     }
 };
 
