d79267f93bf220eb915d35054358178650b82da9
Jacob Middleton <109218193+JacobMiddleton@users.noreply.github.com>
2022-11-09 10:37:01 -0500

    Add an else statement and fixed variable name

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -11,12 +11,16 @@ const Spotify = {
         const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
         const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
 
-        if(accessTokenMatch && expiresIn) {
+        if(accessTokenMatch && expiresInMatch) {
             accessToken = accessTokenMatch[1];
             const expiresIn = Number(expiresInMatch[1]);
             window.setTimeout(() => accessToken = '', expiresIn * 1000);
             window.history.pushState('Access Token', null, '/');
             return accessToken;
+        }
+        else {
+            const redirectURL = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`;
+            window.location = redirectURL;
         }
     }
 };
