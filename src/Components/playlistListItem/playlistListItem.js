720751e9704af4efeace1635199031098623f21b
Jacob Middleton <109218193+JacobMiddleton@users.noreply.github.com>
2022-12-15 11:41:36 -0500

    Add new component

---
+++ src/Components/playlistListItem/playlistListItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './playlistListItem.css';
+
+export class playlistListItem extends React.Component {
+    render() {
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
